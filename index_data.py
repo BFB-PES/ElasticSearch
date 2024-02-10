dba05627a4e99ee276cfb259ed9ae40cfd6727d1
Sayonika Das <64629985+sayonika02@users.noreply.github.com>
2024-02-10 15:21:53 +0530

    upgrade ES to 8.12

--- index_data.py
+++ index_data.py
@@ -1,8 +1,10 @@
 #elastic search with last index data
-
+import pickle
 import psycopg2
 from elasticsearch import Elasticsearch
 import time
+import helpers
+import indexMappings
 
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -13,7 +15,7 @@ postgres_config = {
     'database': 'fashion',
     'user': 'postgres',
     'password': 'postgres',
-    'port': '5432'  
+    'port': '5432'  # Default PostgreSQL port
 }
 
 pg_conn = psycopg2.connect(**postgres_config)
@@ -23,7 +25,7 @@ pg_cursor = pg_conn.cursor()
 es = Elasticsearch(['http://localhost:9200'])
 
 def get_last_indexed_id(index_name):
-    # Query Elasticsearch to get the last record - saves state
+    # Query Elasticsearch to get the last record
     body = {
         "size": 1,
         "sort": [
@@ -39,32 +41,72 @@ def get_last_indexed_id(index_name):
     # Extract the ID of the last record
     last_indexed_id = res['hits']['hits'][0]['_source']['id']
 
-    return last_indexed_id 
+    return last_indexed_id
+
 
 def index_postgresql_to_elasticsearch(table_name, index_name):
-    # Retrieve the ID of the last indexed record
-    last_indexed_id = get_last_indexed_id(index_name)
+    # Check if the index exists
+    if not es.indices.exists(index=index_name):
+        # If the index doesn't exist, create it
+        es.indices.create(index=index_name, mappings=indexMappings.fashion_mappings)
+        print("Index created.")
+        last_indexed_id = 0
+        #index all the records
+        pg_cursor.execute(f"SELECT * FROM fashion1")
+        columns = [desc[0] for desc in pg_cursor.description]
+        rows = pg_cursor.fetchall()
 
-    # Retrieve new records from PostgreSQL
-    pg_cursor.execute(f"SELECT * FROM {table_name} WHERE id > {last_indexed_id}")
-    columns = [desc[0] for desc in pg_cursor.description]
-    rows = pg_cursor.fetchall()
+        # Index data in Elasticsearch
+        limit = 10000
+        i = 0
+        for row in rows:
+            if i<limit:
+                doc = dict(zip(columns, row))
+                es.index(index=index_name, body=doc)
+                i = i + 1
 
-    # Index new records in Elasticsearch
-    for row in rows:
-        doc = dict(zip(columns, row))
-        es.index(index=index_name, body=doc)
+    else:
+        # If the index exists, retrieve the ID of the last indexed record
+        last_indexed_id = get_last_indexed_id(index_name)
+
+        # Retrieve new records from PostgreSQL
+        pg_cursor.execute(f"SELECT * FROM {table_name} WHERE id > {last_indexed_id}")
+        columns = [desc[0] for desc in pg_cursor.description]
+        rows = pg_cursor.fetchall()
+        
+        # Index new records in Elasticsearch
+        #limit = 10000
+        #i = 0 #counter
+        for row in rows:
+            #if i<limit:
+            doc = dict(zip(columns, row))
+            doc["DescriptionVector"] = helpers.get_description_vector(doc["name"])
+            with open('seller_list.pkl', 'rb') as f:
+                seller_list = pickle.load(f)
+                if doc["seller"] not in seller_list:
+                    seller_list.add(doc["seller"])
+            try:
+                es.index(index = index_name, document=doc, id=doc["id"])
+            except Exception as e:
+                print(e)
+            #i = i+1
+            
+        
+    print("Index count: ", es.count(index = index_name))
 
 
-#check: no hit situation; when no record match occurs
+
+
 
 if __name__ == '__main__':
     print("Started")
     # Specify the PostgreSQL table name and Elasticsearch index name
     postgresql_table = 'fashion0'
-    elasticsearch_index = 'fashion_index_1'
+    elasticsearch_index = 'fashion_v2'
 
     start_time = time.time()
+    #if es.indices.exists(index=elasticsearch_index):
+    print(f"printing index name: {elasticsearch_index}")
     index_postgresql_to_elasticsearch(postgresql_table, elasticsearch_index)
     end_time = time.time()
     elapsed_time = end_time - start_time
